b3aad9a5a4f85132c24a00a1f0978adb9c4ddb89
gio <giovanniburoni@giovannis-MacBook-Pro.local>
2022-03-18 10:47:35 +0100

    add sidebar

--- ML-unsup/gmm.py
+++ ML-unsup/gmm.py
@@ -7,32 +7,34 @@ import random
 import streamlit as st
 from scipy.stats import multivariate_normal
 
-@st.cache
-def generate_random_number():
-    return random.randint(1,1000)
+
 
 
 st.title('Gaussian Mixture Model')
 
-seed = generate_random_number()
+
+
+st.sidebar.markdown("## Find the number of cluster: ")
+
+seed = st.sidebar.slider('SEED', 0, 1000, 1)
+
 st.subheader('SEED: ' + str(seed))
-# generate random sample, two components
 np.random.seed(seed)
 random.seed(seed)
-
-n_samples = st.selectbox( 'NUMBER SAMPLES', (100, 500, 1000))
-gmm_components = st.selectbox( 'GMM COMPONENTS NUMBER', (2, 3, 4, 5))
-ITER = st.selectbox( 'MAX ITER', (10, 20, 50, 100))
-
-n = random.randint(2, 5)
+n_samples = random.randint(500, 1000)
+n = random.randint(2, 7)
 interval = random.randint(1, 3)
 
+st.sidebar.markdown("## GMM Hyper-parameters: ")
+GMM_COMP = st.sidebar.slider( 'GMM COMPONENTS NUMBER', 1, 7, 1)
+ITER = st.sidebar.selectbox( 'MAX ITER', (10, 20, 50, 100))
+
+
+st.header('Dataset Generated')
 
 list_gaussian = []
 
 for i in range(0, n):
-
-    print(i)
 
     C = np.array([[random.uniform(-interval, interval), random.uniform(-interval, interval)], [random.uniform(-interval, interval), random.uniform(-interval, interval)]])
 
@@ -44,14 +46,27 @@ for i in range(0, n):
 
 X_train = np.vstack(list_gaussian)
 
+# plot samples
+fig, ax = plt.subplots(figsize=(15, 10))
+plt.scatter( X_train[:, 0], X_train[:, 1], 0.8)
+st.pyplot(plt)
+
+st.markdown("---")
+st.header('GMM Fit')
 
 # fit a Gaussian Mixture Model with two components
-clf = mixture.GaussianMixture(n_components = gmm_components, covariance_type = "full", max_iter = ITER)
+clf = mixture.GaussianMixture(n_components = GMM_COMP, covariance_type = "full", max_iter = ITER)
 clf.fit(X_train)
-
 BIC = clf.bic(X_train)
 AIC = clf.aic(X_train)
 
+col1, col2 = st.columns([1, 2])
+
+with col1:
+    st.text(f"BIC :  {BIC:.2f}")
+
+with col2:
+    st.text(f"AIC: {AIC:.2f}")
 
 # display predicted scores by the model as a contour plot
 x = np.linspace(-50.0, 50.0)
@@ -63,14 +78,13 @@ XX = np.array([X.ravel(), Y.ravel()]).T
 Z = - clf.score_samples(XX)
 Z = Z.reshape(X.shape)
 
-fig, ax = plt.subplots(figsize=(15, 10))
+
 
 CS = plt.contour( X, Y, Z, norm = LogNorm( vmin = 1.0, vmax = 500.0 ), levels = np.logspace(0, 3, 100)) # 
 
-# plot samples
-plt.scatter( X_train[:, 0], X_train[:, 1], 0.8)
 
-for comp in range(gmm_components):
+
+for comp in range(GMM_COMP):
 
     mu = clf.means_[comp]
 
@@ -80,18 +94,25 @@ for comp in range(gmm_components):
 
     neg_log = - rv.logpdf(pos)
 
-    plt.contour(x, y, neg_log , norm = LogNorm( vmin = 1.0, vmax = 100.0 ), levels = np.logspace(0, 3, 10), cmap='RdGy')
+    plt.contour(x, y, neg_log , norm = LogNorm( vmin = 1.0, vmax = 100.0 ), levels = np.logspace(0, 5, 10), cmap='RdGy')
 
     plt.scatter( mu[0], mu[1], color = "darkorange")
 
-plt.title("Negative log-likelihood predicted by a GMM", fontsize=20)
-plt.suptitle(f"BIC :  {BIC:.2f}          AIC: {AIC:.2f}", fontsize=20)
+plt.suptitle("Negative log-likelihood predicted by a GMM", fontsize=20)
 plt.axis("tight")
 plt.show()
 st.pyplot(plt)
 
-st.header(' Discover the true number of Gaussian distributions generating the data ')
+st.markdown("---")
+st.header(' Discover what the number is ')
 if st.checkbox('Click me'):
-    st.subheader(f"! {n}")
+    
+    st.subheader(f'The number of Gaussina distributions generating the data: - {n} -')
+    # plot samples
+    fig, ax = plt.subplots(figsize=(15, 10))
+    for i in range(0, n):
+            plt.scatter( X_train[ i * n_samples : (i+1) * n_samples , 0], X_train[ i * n_samples : (i+1) * n_samples, 1], 0.8)
+
+    st.pyplot(plt)
 
 
